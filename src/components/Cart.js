7a361c074277a7e3cd70b10fd38fa43782caed08
Firman <fblazt@outlook.com>
2020-05-14 04:16:37 +0700

    Add empty cart component

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -7,11 +7,6 @@ export default function Cart() {
         Cart
             <span className='ml-2 bg-blue-200 px-2 text-white rounded-full'>0</span>
       </div>
-      <div className='hidden flex flex-col items-center'>
-        <img src={require("../assets/img/food-and-restaurant.png")} alt="No item" />
-        <p className='text-xl font-extrabold'>Your cart is empty</p>
-        <p>Please add some items from the menu</p>
-      </div>
       <div className='pt-24 pb-5 px-5 h-full flex flex-col justify-between'>
         <div>
           <div className='flex flex-row'>
