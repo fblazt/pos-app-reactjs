4dfa8764c9e6e7735ff6ecd9acc8b78a54ef9f98
Firman <fblazt@outlook.com>
2020-05-14 04:27:57 +0700

    Add cart footer component

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import CartItem from './CartItem'
+import CartFooter from './CartFooter'
 
 export default function Cart() {
   return (
@@ -10,15 +11,7 @@ export default function Cart() {
       </div>
       <div className='pt-24 pb-5 px-5 h-full flex flex-col justify-between'>
         <CartItem/>
-        <div className='flex flex-col'>
-          <span className='flex flex-row justify-between text-xl font-bold'>
-            <p>Total:</p>
-            <p>Rp. 5.000*</p>
-          </span>
-          <p>*Belum termasuk ppn</p>
-          <button className='py-3 mb-3 bg-blue-400 text-white text-2xl font-semibold'>Checkout</button>
-          <button className='py-3 bg-pink-400 text-white text-2xl font-semibold'>Cancel</button>
-        </div>
+        <CartFooter/>
       </div>
     </aside>
   )
---
+++ src/components/CartFooter.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export default function CartFooter() {
+  return (
+    <div className='flex flex-col'>
+      <span className='flex flex-row justify-between text-xl font-bold'>
+        <p>Total:</p>
+        <p>Rp. 5.000*</p>
+      </span>
+      <p>*Belum termasuk ppn</p>
+      <button className='py-3 mb-3 bg-blue-400 text-white text-2xl font-semibold'>Checkout</button>
+      <button className='py-3 bg-pink-400 text-white text-2xl font-semibold'>Cancel</button>
+    </div>
+  )
+}
